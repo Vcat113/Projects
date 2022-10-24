fd4d0a7cf1e5323c2b411db910abcfcc9e0b8f9a
Vcat113 <112778454+Vcat113@users.noreply.github.com>
2022-10-24 17:37:45 -0500

    Add files via upload
    Added foreign key.

--- RevievDatabase.sql
+++ RevievDatabase.sql
@@ -2,19 +2,23 @@ drop database Review;
 create database Review;
 use Review;
 
-create table GReview(
-RevNo int primary key,
-TRev text,
-NeoReview int
-);
 create table Login(
 sId int primary key,
 sName varchar(255),
 sUser varchar(255),
 sPassword varchar(255)
 );
+create table GReview(
+RevNo int,
+UId int,
+TRev text,
+NeoReview int,
+foreign key(UId) references Login(sId),
+primary key(RevNo,UId)
+);
+
 insert into Log1 (sId, sName, sUser, sPassword)
-values (1, 'Ven Light', 'Vlig1',  'Password');
+values (1, 'Ven Light', 'Vlig1',  'Password'), (2, 'Til S', 'TilS1', 'Password1');
 
 select * from staff;
 
